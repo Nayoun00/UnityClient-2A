eafc9c087725dccb1c91554cc519e59f796c3145
Nayoun00 <nayounseok12@gmail.com>
2024-04-30 09:52:22 +0900

    Feat : 게임 틀 정리 시작
    구현 구성 확인 및 GameSystem 스크립트 재작성

---
+++ UnityClient-2A_2024_1/Assets/Scripts/StoryGame/GameSystem.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.Text;
+#if UNITY_EDITOR
+public class GameSystemEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        GameSystem gameSystem = (GameSystem)target;
+
+        if(GUILayout.Button("Reset Story Models"))      //에디터에서 버튼 생성
+        {
+            gameSystem.ResetStoryModels();
+        }
+    }
+
+}
+#endif
+public class GameSystem : MonoBehaviour
+{
+    public StoryModel[] storyModels;                    //변경된 스토리 모델로 생성
+
+#if UNITY_EDITOR
+    [ContextMenu("Reset Story Models")]
+    public void ResetStoryModels()
+    {
+        storyModels = Resources.LoadAll<StoryModel>(""); //Resources 폴더 아래 모든 StoryModel 불러오기
+    }
+#endif
+}
